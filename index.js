e8001ce6894e9897c059ba7a9453d3eefde44a16
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-07-04 12:40:16 +0545

    working on search funcionality

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ window.addEventListener("load", () => {
    }
 
    function showWeatherData(data){
-        let {humidity,temp, pressure} = data.current;
+        let {temp} = data.current;
         let {timezone, lat, lon} = data;
         let {description} = data.current.weather[0];
         console.log(lon);
@@ -41,7 +41,39 @@ window.addEventListener("load", () => {
        
      }
 
+
+     document.querySelector("#searchbutton").addEventListener("click" , searchlocation);
+     
+     
+     function searchlocation() {
+         let search = document.getElementById("search").value;
+         console.log(search);
+         console.log("search button clicked")
+         const API_KEY = `7f11cdd7f1de8797d5aa63f1c9da4679`
+         const api =  `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}`
+         fetch(api).then( response => {
+             return response.json();
+         })
+         
+         .then(data => {
+             console.log(data);
+             showWeatherDataByLocation(data);
+         })
+     }
+     
+     showWeatherDataByLocation(data) {
+        let {temp} = data.main;
+        let {name} = data.main;
+        let {description} = data.weather[0];
+        $('#icon').attr("src","http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png");
+
+        document.getElementById("timezone").innerHTML = name;
+        document.getElementById("temp").innerHTML = temp;
+        // document.getElementById("humidity").innerHTML = "Humidity: " +humidity;
+        // document.getElementById("pressure").innerHTML = "Pressure: " +pressure;
+        document.getElementById("weatherdesc").innerHTML = capitalize(description); 
        
+     }
    
 
    
